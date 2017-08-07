b86da6c583e924e0d90ff52963dbcc4d51078dab
laixhe <laiki@laixhe.com>
2017-08-07 14:33:05 +0800

    更新错误和异常类，等待下一步编写

--- library/debug/ExceptionRegister.php
+++ library/debug/ExceptionRegister.php
@@ -6,77 +6,6 @@ namespace library\debug;
  */
 class ExceptionRegister
 {
-    /**
-     * @var array HTTP返回码
-     */
-    private static $httpCode = [
-        100 => 'Continue',
-        101 => 'Switching Protocols',
-        102 => 'Processing',
-        118 => 'Connection timed out',
-        200 => 'OK',
-        201 => 'Created',
-        202 => 'Accepted',
-        203 => 'Non-Authoritative',
-        204 => 'No Content',
-        205 => 'Reset Content',
-        206 => 'Partial Content',
-        207 => 'Multi-Status',
-        208 => 'Already Reported',
-        210 => 'Content Different',
-        226 => 'IM Used',
-        300 => 'Multiple Choices',
-        301 => 'Moved Permanently',
-        302 => 'Found',
-        303 => 'See Other',
-        304 => 'Not Modified',
-        305 => 'Use Proxy',
-        306 => 'Reserved',
-        307 => 'Temporary Redirect',
-        308 => 'Permanent Redirect',
-        310 => 'Too many Redirect',
-        400 => 'Bad Request',
-        401 => 'Unauthorized',
-        402 => 'Payment Required',
-        403 => 'Forbidden',
-        404 => 'Not Found',
-        405 => 'Method Not Allowed',
-        406 => 'Not Acceptable',
-        407 => 'Proxy Authentication Required',
-        408 => 'Request Time-out',
-        409 => 'Conflict',
-        410 => 'Gone',
-        411 => 'Length Required',
-        412 => 'Precondition Failed',
-        413 => 'Request Entity Too Large',
-        414 => 'Request-URI Too Long',
-        415 => 'Unsupported Media Type',
-        416 => 'Requested range unsatisfiable',
-        417 => 'Expectation failed',
-        418 => 'I\'m a teapot',
-        421 => 'Misdirected Request',
-        422 => 'Unprocessable entity',
-        423 => 'Locked',
-        424 => 'Method failure',
-        425 => 'Unordered Collection',
-        426 => 'Upgrade Required',
-        428 => 'Precondition Required',
-        429 => 'Too Many Requests',
-        431 => 'Request Header Fields Too Large',
-        449 => 'Retry With',
-        450 => 'Blocked by Windows Parental Controls',
-        500 => 'Internal Server Error',
-        501 => 'Not Implemented',
-        502 => 'Bad Gateway or Proxy Error',
-        503 => 'Service Unavailable',
-        504 => 'Gateway Time-out',
-        505 => 'HTTP Version not supported',
-        507 => 'Insufficient storage',
-        508 => 'Loop Detected',
-        509 => 'Bandwidth Limit Exceeded',
-        510 => 'Not Extended',
-        511 => 'Network Authentication Required',
-    ];
 
     /**
      * 注册错误/异常处理函数
@@ -106,7 +35,6 @@ class ExceptionRegister
      */
     public function exceptionHandler($e)
     {
-        print_r($e);
     }
 
     /**
@@ -115,13 +43,9 @@ class ExceptionRegister
      * @param string $errstr 错误的信息
      * @param string $errfile 发生错误的文件名
      * @param integer $errline 错误发生的行号
-     * @return null|boolean 是否继续执行默认的错误处理
-     * @throws \ErrorException 抛出一个错误异常
      */
     public function errorHandler($errno, $errstr, $errfile, $errline)
     {
-        print_r(error_reporting());
-        throw new \ErrorException($errstr, $errno, $errno, $errfile, $errline);
     }
 
     /**
@@ -129,8 +53,6 @@ class ExceptionRegister
      */
     public function shutdownHandler()
     {
-        //获取最后发生的错误
-        print_r(error_get_last());
     }
 
 }
